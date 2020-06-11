84cb50c796cc32faafbc52546fa5e8a64121089f
monpro <monproshen@gmail.com>
2020-06-11 22:17:24 +1000

    add Executor Example (with a thread)

---
+++ src/com/monpro/thread/Executor/Main.java
@@ -0,0 +1,30 @@
+package com.monpro.thread.Executor;
+
+import java.util.Random;
+import java.util.concurrent.*;
+
+public class Main {
+    static class Task implements Callable<Integer>{
+        @Override
+        public Integer call() throws Exception {
+            int sleepSeconds = new Random().nextInt(1000);
+            Thread.sleep(sleepSeconds);
+            return sleepSeconds;
+        }
+    }
+
+
+    public static void main(String[] args) throws InterruptedException {
+        ExecutorService executor = new ExecutorService();
+        Future<Integer> future = executor.submit(new Task());
+
+        //other task
+        Thread.sleep(1000);
+        try{
+            System.out.println(future.get());
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+        executor.shutdown();
+    }
+}
