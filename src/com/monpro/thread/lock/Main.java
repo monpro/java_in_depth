69276cdd686014035cccf6787baed12cae864b6e
monpro <monproshen@gmail.com>
2020-06-14 20:56:53 +1000

    add atomic example

---
+++ src/com/monpro/thread/lock/Main.java
@@ -0,0 +1,31 @@
+package com.monpro.thread.lock;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class Main {
+    private static AtomicInteger counter = new AtomicInteger(0);
+    static class Counter extends Thread{
+        @Override
+        public void run() {
+            for(int i = 0; i < 1000; i++){
+                counter.incrementAndGet();
+            }
+        }
+    }
+
+
+    public static void main(String[] args) throws InterruptedException {
+        int num = 10000;
+        Thread[] threads = new Thread[num];
+        for(int i = 0; i < num; i++){
+            threads[i] = new Counter();
+            threads[i].start();
+        }
+        for(int i = 0; i < num; i++){
+            threads[i].join();
+        }
+
+        System.out.println(counter.get());
+    }
+
+}
