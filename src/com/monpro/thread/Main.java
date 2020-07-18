322b81de09c971429789a3bc1ebdb2b608227e8f
monpro <monproshen@gmail.com>
2020-07-18 10:25:11 +1000

    add thread poll out of memory example

--- src/com/monpro/thread/Main.java
+++
@@ -1,8 +0,0 @@
-package com.monpro.thread;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Thread Example");
-
-    }
-}
--- src/com/monpro/thread/future/Future.java
+++
@@ -1,5 +0,0 @@
-package com.monpro.thread.Future;
-
-public interface Future<V> {
-    V get() throws Exception;
-}
