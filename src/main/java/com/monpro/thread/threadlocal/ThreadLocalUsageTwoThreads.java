719e3b4046c67334e5aa1854152788de1186a78b
monpro <monproshen@gmail.com>
2020-12-13 13:50:24 +1100

    add lock and global dataFormat object to solve the thread-unsafe issue

--- src/main/java/com/monpro/thread/threadlocal/ThreadLocalUsageTwoThreads.java
+++ src/main/java/com/monpro/thread/threadlocal/ThreadLocalUsageTwoThreads.java
@@ -10,6 +10,9 @@ import java.util.concurrent.Executors;
 public class ThreadLocalUsageTwoThreads {
 
     public static ExecutorService threadPool = Executors.newFixedThreadPool(10);
+    // if use global object, not thread safe - all threads use the same object
+    // we could use Lock to enable thread safe
+    static SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
 
     public static void main(String[] args) {
         for(int i = 0; i < 1000; i++) {
@@ -27,7 +30,10 @@ public class ThreadLocalUsageTwoThreads {
 
     public String getDate(int seconds) {
         Date date = new Date(1000 * seconds);
-        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
-        return dateFormat.format(date);
+        String result = "";
+        synchronized (ThreadLocalUsageTwoThreads.class) {
+            result = dateFormat.format(date);
+        }
+        return result;
     }
 }
