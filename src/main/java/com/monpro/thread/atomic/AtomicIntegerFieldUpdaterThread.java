a88eb377b8c86b4201b812b0ad8e09f1cf9d0383
monpro <monproshen@gmail.com>
2021-01-01 15:09:55 +1100

    add atomic integer field updater use case ( to upgrade basic fields in class )

---
+++ src/main/java/com/monpro/thread/atomic/AtomicIntegerFieldUpdaterThread.java
@@ -0,0 +1,40 @@
+package com.monpro.thread.atomic;
+
+import java.util.concurrent.atomic.AtomicIntegerFieldUpdater;
+
+public class AtomicIntegerFieldUpdaterThread implements Runnable {
+
+    public static Candidate leader;
+    public static Candidate worker;
+
+    public static AtomicIntegerFieldUpdater<Candidate> updater =
+            AtomicIntegerFieldUpdater.newUpdater(Candidate.class, "score");
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 10000; i++) {
+            leader.score += 1;
+            updater.getAndIncrement(worker);
+        }
+    }
+
+    static class Candidate {
+        volatile int score;
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        leader = new Candidate();
+        worker = new Candidate();
+        AtomicIntegerFieldUpdaterThread updaterThread = new AtomicIntegerFieldUpdaterThread();
+        Thread thread1 = new Thread(updaterThread);
+        Thread thread2 = new Thread(updaterThread);
+        thread1.start();
+        thread2.start();
+        thread1.join();
+        thread2.join();
+        System.out.println("leader score: " + leader.score);
+        System.out.println("worker score(with updater): " + worker.score);
+
+    }
+
+}
