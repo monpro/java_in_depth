6be309400ca87d08c43535ca1d6b8d62cfaa298d
monpro <monproshen@gmail.com>
2021-01-08 14:45:49 +1100

    add implementation of minStack

---
+++ src/main/java/com/monpro/generics/stack/MinStack.java
@@ -0,0 +1,37 @@
+package com.monpro.generics.stack;
+
+import java.util.Stack;
+
+public class MinStack {
+
+    /** initialize your data structure here. */
+    Stack<Integer> stack, minStack;
+    public MinStack() {
+        stack = new Stack<Integer>();
+        minStack = new Stack<Integer>();
+    }
+
+    public void push(int x) {
+        stack.add(x);
+        if (minStack.isEmpty()) {
+            minStack.add(x);
+        } else if (minStack.peek() >= x) {
+            minStack.add(x);
+        }
+    }
+
+    public void pop() {
+        int element = stack.pop();
+        if (element == minStack.peek()) {
+            minStack.pop();
+        }
+    }
+
+    public int top() {
+        return stack.peek();
+    }
+
+    public int getMin() {
+        return minStack.peek();
+    }
+}
