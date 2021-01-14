2d8db902ffd7d64d97227da2ac6b06ff60cddde3
monpro <monproshen@gmail.com>
2021-01-14 17:33:48 +1100

    add ListUtils & swapNodes & test cases

--- src/main/java/com/monpro/generics/util/ListUtils.java
+++ src/main/java/com/monpro/generics/util/ListUtils.java
@@ -30,4 +30,53 @@ public class ListUtils {
         newHead.next = nextNode;
         return dummy.next;
     }
+
+    public static ListNode swapNodes(ListNode head, int k) {
+        int length = 0;
+        ListNode dummy = new ListNode(-1);
+        dummy.next = head;
+
+        ListNode node = dummy;
+        while(node.next != null) {
+            node = node.next;
+            length += 1;
+        }
+
+        // reset
+        ListNode firstKNode = null, lastKNode = null;
+        ListNode nextFirstKNode = null, nextLastKNode = null;
+        node = dummy;
+        ListNode prevFirstKNode = null, prevLastKNode = null;
+        for(int i = 0; i < k; i++) {
+            prevFirstKNode = node;
+            node = node.next;
+        }
+        firstKNode = node;
+        nextFirstKNode = node.next;
+
+        node = dummy;
+        for(int i = 0; i < length - k + 1; i++) {
+            prevLastKNode = node;
+            node = node.next;
+        }
+        lastKNode = node;
+        nextLastKNode = node.next;
+        // System.out.println(prevFirstKNode.val + " " + firstKNode.val + " " + nextFirstKNode.val);
+        // System.out.println(prevLastKNode.val + " " + lastKNode.val + " " + nextLastKNode.val);
+        if(firstKNode.next == lastKNode) {
+            prevFirstKNode.next = lastKNode;
+            lastKNode.next = firstKNode;
+            firstKNode.next = nextLastKNode;
+        } else if(lastKNode.next == firstKNode) {
+            prevLastKNode.next = firstKNode;
+            firstKNode.next = lastKNode;
+            lastKNode.next = nextFirstKNode;
+        }else {
+            prevFirstKNode.next = lastKNode;
+            lastKNode.next = nextFirstKNode;
+            prevLastKNode.next = firstKNode;
+            firstKNode.next = nextLastKNode;
+        }
+        return dummy.next;
+    }
 }
--- src/test/java/com/monpro/generics/util/ListUtilsTest.java
+++ src/test/java/com/monpro/generics/util/ListUtilsTest.java
@@ -18,6 +18,19 @@ public class ListUtilsTest {
         ListUtils.MergeListBetweenNodes(head1, 1, 2, head2);
         assertEquals(head1.next.val, 7);
         assertEquals(head1.next.next.val, 8);
+    }
 
+    @Test
+    void TestSwapNodes() {
+        ListNode head1 = new ListNode(1);
+        head1.next = new ListNode(2);
+        head1.next.next = new ListNode(3);
+        head1.next.next.next = new ListNode(4);
+        head1.next.next.next.next = new ListNode(5);
+
+        ListUtils.swapNodes(head1, 2);
+        assertEquals(head1.next.val, 4);
+        assertEquals(head1.next.next.val, 3);
+        assertEquals(head1.next.next.next.val, 2);
     }
 }
