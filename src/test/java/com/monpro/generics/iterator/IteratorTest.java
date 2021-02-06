f36df60226754764ce934b1f08c117dfdccb0698
monpro <monproshen@gmail.com>
2021-02-06 19:58:01 +1100

    add peeking iterator & use next variable to preserve the peeking element  & test case

---
+++ src/test/java/com/monpro/generics/iterator/IteratorTest.java
@@ -0,0 +1,20 @@
+package com.monpro.generics.iterator;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class IteratorTest {
+    @Test
+    void PeekingIteratorTest() {
+        Iterator<Integer> iterator = new ArrayList<Integer>(Arrays.asList(1, 2, 3, 4, 5)).iterator();
+        PeekingIterator<Integer> peekingIterator = new PeekingIterator<>(iterator);
+        assertEquals(peekingIterator.peek(), 1);
+        assertEquals(peekingIterator.next(), 1);
+        assertEquals(peekingIterator.hasNext(), true);
+        assertEquals(peekingIterator.peek(), 2);
+    }
+}
